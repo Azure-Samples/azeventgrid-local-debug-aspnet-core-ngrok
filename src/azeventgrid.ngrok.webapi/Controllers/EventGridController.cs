637662e3c80eca997e4f9644a2fd58c8284cf3a8
Tianyun Gao <v-tianga@microsoft.com>
2021-08-26 14:50:07 +0800

    Update sdk version

--- src/azeventgrid.ngrok.webapi/Controllers/EventGridController.cs
+++ src/azeventgrid.ngrok.webapi/Controllers/EventGridController.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Messaging.EventGrid;
 using Azure.Messaging.EventGrid.SystemEvents;
