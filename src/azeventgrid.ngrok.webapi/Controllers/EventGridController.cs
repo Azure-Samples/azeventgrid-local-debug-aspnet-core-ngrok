7aa04861a68ffdb38b6c6657df28f2e677bb2c92
Yun Lu (MSFT) <53427149+Luyunmt@users.noreply.github.com>
2021-08-30 06:23:56 +0800

    Change event grid track1 to track2 (#3)
    * changee eventgrid track1 to track2
    
    * changee eventgrid track1 to track2
    
    * change according to comments
    
    * update package version to GA
    
    * update package version to GA
    
    * update package version to GA
    
    * Update sdk version
    
    * Update code
    
    * Update code
    
    Co-authored-by: Tianyun Gao <v-tianga@microsoft.com>

--- src/azeventgrid.ngrok.webapi/Controllers/EventGridController.cs
+++ src/azeventgrid.ngrok.webapi/Controllers/EventGridController.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.EventGrid;
-using Microsoft.Azure.EventGrid.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace azeventgrid.ngrok.webapi.Controllers
 {
@@ -29,27 +26,29 @@ namespace azeventgrid.ngrok.webapi.Controllers
                 using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
                 {
                     var jsonContent = await reader.ReadToEndAsync();
-                    var eventGridEvents = JsonConvert.DeserializeObject<List<EventGridEvent>>(jsonContent);
+                    var eventGridEvents = EventGridEvent.ParseMany(BinaryData.FromString(jsonContent));
 
-                    foreach (var eventGridEvent in eventGridEvents)
+                    foreach (var egEvent in eventGridEvents)
                     {
-                        // EventGrid validation message
-                        if (eventGridEvent.EventType == EventTypes.EventGridSubscriptionValidationEvent)
+                        if (egEvent.TryGetSystemEventData(out object data))
                         {
-                            var eventData = ((JObject)(eventGridEvent.Data)).ToObject<SubscriptionValidationEventData>();
-                            var responseData = new SubscriptionValidationResponse()
+                            // EventGrid validation message
+                            if (data is SubscriptionValidationEventData subscriptionData)
                             {
-                                ValidationResponse = eventData.ValidationCode
-                            };
-                            return Ok(responseData);
+                                var responseData = new SubscriptionValidationResponse()
+                                {
+                                    ValidationResponse = subscriptionData.ValidationCode
+                                };
+                                return Ok(responseData);
+                            }
                         }
                         // handle all other events
-                        await this.HandleEvent(eventGridEvent);
+                        await this.HandleEvent(egEvent);
                         return result;
                     }
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
@@ -58,7 +57,7 @@ namespace azeventgrid.ngrok.webapi.Controllers
         }
         private async Task<IActionResult> HandleEvent(EventGridEvent eventGridEvent)
         {
-            if (eventGridEvent.EventType == EventTypes.StorageBlobCreatedEvent)
+            if (eventGridEvent.EventType == SystemEventNames.StorageBlobCreated)
             {
                 // do something
             }
